53063c8fa7be5957c343963c2f9223926ac2e035
AleksSkyr <greshnik1499@gmail.com>
2023-09-30 14:48:37 +0300

    startCursovik2

---
+++ demo/src/test/java/Cursovik/demo/CursApplicationTests.java
@@ -0,0 +1,13 @@
+package Cursovik.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
